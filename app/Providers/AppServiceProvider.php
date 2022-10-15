9a1716f0c8a15811cd60bf71218dad2998913eca
ryo <ryotan111haru@gmail.com>
2022-10-16 04:37:33 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+
+        if(\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
